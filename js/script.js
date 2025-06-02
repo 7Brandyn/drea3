4d6f7f0e5a931b8473b5d951f0c83c66acdf8210
brandyn.evans7@gmail.com <brandyn.evans7@gmail.com>
2025-06-02 14:06:43 -0700

    added drea-3 experience

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const interval = setInterval(() => {
+    const viewer = document.querySelector('spline-viewer');
+    if (viewer && viewer.shadowRoot) {
+        const logo = viewer.shadowRoot.querySelector('#logo');
+        if (logo) {
+            logo.remove(); // 💥 remove the logo element entirely
+            console.log("nothing to see here");
+            clearInterval(interval);
+        }
+    }
+}, 500);
